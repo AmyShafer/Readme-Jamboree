706a40b0917768b3a5e5c1316404f2b0f02ca348
Amy Shafer <amy.u.shafer@gmail.com>
2022-02-28 17:22:42 -0500

    removed console.logs

--- init/index.js
+++ init/index.js
@@ -1,27 +1,12 @@
-// ✅ a command-line application that accepts user input
-// ✅ I am prompted for information about my application repository
-// ✅ a high-quality, professional README.md is generated with the title of my project and sections entitled ✅ Description, ✅ Table of Contents, ✅ Installation, ✅ Usage, ✅ License, ✅ Contributing, ✅ Tests, and ✅ Questions
-// ✅ I enter my project title
-// ✅ this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// ✅  I choose a license for my application from a list of options
-// ✅  a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// ✅ I enter my GitHub username
-// ✅ this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// ✅ I enter my email address
-// ✅ this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// ✅ I click on the links in the Table of Contents
-// ✅ I am taken to the corresponding section of the README
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateReadme = require('./generateReadme.js');
 const licenseInfo = require('./licenses.js');
 
+// creates readme based on user input
 const readmeJamboree = function ({ title, description, instructions, usage, license, contributing, tests, github, email }) {
-  console.log(license);
-  const licenseIndex = licenseInfo(license);
+  licenseInfo(license);
   return `
 # ${title}
 
@@ -66,6 +51,7 @@ If you have any questions, you can visit my GitHub Page at <a href="https://gith
 You can also email me at ${email}.
 `};
 
+// runs the program
 const init = () => {
   generateReadme()
   // Use File System to generate the readme file
