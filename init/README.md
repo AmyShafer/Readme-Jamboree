3c959c4549e90192c775743c7b5c887314d2b8a8
Amy Shafer <amy.u.shafer@gmail.com>
2022-02-27 19:03:26 -0500

    added more sections

--- init/README.md
+++ init/README.md
@@ -1,43 +1,43 @@
 
-# Richard Simmons
+# Sweatin' to the Oldies
 
-Milton Teagle
+Get ready for 25-minutes of low-impact aerobics.
 
 ## Table of contents
-* [Instructions](#instructions)
+* [Install-Instructions](#install-instructions)
 * [Usage](#usage)
 * [License](#license)
 * [Contributing](#contributing)
 * [Tests](#tests)
 * [Questions](#questions)
+* [![Made for - Penn LPS](https://img.shields.io/static/v1?label=Made+for&message=Penn+LPS&color=%23FF1493)](https://bootcamp.sas.upenn.edu/)
+* [![made-with-javascript](https://img.shields.io/badge/Made%20with-JavaScript-1f425f.svg)](https://www.javascript.com)
 
-## Instructions
+## Install Instructions
 
-This website was built from scratch using undefined!
+Be sure to warm-up with us before attempting the more strenuous parts of the DVD or VHS tape.
 
 ## Usage
 
 Here's a look at the application in action:
 
-<img src="Get Ready for 25 minutes of low impact aerobics!" width="490" height="340" alt="Richard Simmons/>
+<img src="richard.png" width="490" height="340" alt="Sweatin' to the Oldies/>
 
 ## License
 
-
+undefined
+undefined
 
 ## Contributing
 
-Users should have fun with the application.
-
-Users should have fun with the application.
-
-Users should have fun with the application.
-
----
+Users should stay hydrated during their exercise.
 
 ## Tests
-undefined
+
+Ok, sure.
 
 ## Questions
-AmyShafer
-amy.u.shafer@gmail.com
+
+If you have any questions, you can visit my GitHub Page at <a href="https://github.com/milton-teagle" target="_blank">milton-teagle</a>.
+
+You can also email me at <richard-simmons@gmail.com>.
--- init/index.js
+++ init/index.js
@@ -1,23 +1,23 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
+// ✅ a command-line application that accepts user input
+// ✅ I am prompted for information about my application repository
+// ✅ a high-quality, professional README.md is generated with the title of my project and sections entitled ✅ Description, ✅ Table of Contents, ✅ Installation, ✅ Usage, ✅ License, ✅ Contributing, ✅ Tests, and ✅ Questions
+// ✅ I enter my project title
+// ✅ this is displayed as the title of the README
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+// ✅  I choose a license for my application from a list of options
+// ✅  a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// ✅ I enter my GitHub username
+// ✅ this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// ✅ I enter my email address
+// ✅ this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// ✅ I click on the links in the Table of Contents
+// ✅ I am taken to the corresponding section of the README
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const readmeJamboree = ({ title, description, instructions, usage, license, contributing, tests, github, email}) => 
+const readmeJamboree = ({ title, description, instructions, usage, license, contributing, tests, github, email }) => 
 
 `
 # ${title}
@@ -25,16 +25,18 @@ const readmeJamboree = ({ title, description, instructions, usage, license, cont
 ${description}
 
 ## Table of contents
-* [Instructions](#instructions)
+* [Install-Instructions](#install-instructions)
 * [Usage](#usage)
 * [License](#license)
 * [Contributing](#contributing)
 * [Tests](#tests)
 * [Questions](#questions)
+* [![Made for - Penn LPS](https://img.shields.io/static/v1?label=Made+for&message=Penn+LPS&color=%23FF1493)](https://bootcamp.sas.upenn.edu/)
+* [![made-with-javascript](https://img.shields.io/badge/Made%20with-JavaScript-1f425f.svg)](https://www.javascript.com)
 
-## Instructions
+## Install Instructions
 
-This website was built from scratch using ${instructions}!
+${instructions}
 
 ## Usage
 
@@ -44,24 +46,22 @@ Here's a look at the application in action:
 
 ## License
 
-${license}
+${license.name}
+${license.badge}
 
 ## Contributing
 
-${contributing}.
-
-${contributing}.
-
-${contributing}.
-
----
+${contributing}
 
 ## Tests
+
 ${tests}
 
 ## Questions
-${github}
-${email}
+
+If you have any questions, you can visit my GitHub Page at <a href="https://github.com/${github}" target="_blank">${github}</a>.
+
+You can also email me at <${email}>.
 `;
 
 // Pull data from the uses inputs to dynamically create a readme file
@@ -79,7 +79,12 @@ const generateReadme = () => {
     },
     {
       type: 'input',
-      message: 'Provide instructions and examples for use:',
+      message: 'Please inform the user how to install the application:',
+      name: 'instructions',    
+    },
+    {
+      type: 'input',
+      message: 'Provide the file name for a screenshot of your project',
       name: 'usage',    
     },
     {
@@ -89,24 +94,30 @@ const generateReadme = () => {
       choices: [
         {
           name: 'MIT',
+          badge: '* [![GitHub license](https://badgen.net/github/license/Naereen/Strapdown.js)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)',
+          section: 'MIT License - Copyright (c) [2022] - Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the Software), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. - THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
         },
         {
           name: 'Apache License',
+          badge: '* [![Npm package license](https://badgen.net/npm/llicense/discord.js)](https://npmjs.com/package/discord.js)',
+          section: 'Copyright [2022] - Licensed under the Apache License, Version 2.0 (the "License") - You may not use this file except in compliance with the License. You may obtain a copy of the License at - http://www.apache.org/licenses/LICENSE-2.0 - Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an AS IS BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.',
         },
         {
           name: 'GPL License', 
-        }
-    ],
+          badge: '* [![Npm package license](https://badgen.net/npm/llicense/discord.js)](https://npmjs.com/package/discord.js)',
+          section: 'GNU GENERAL PUBLIC LICENSE - Version 3, - Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/> - Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.'
+        },
+     ],
     },
     {
       type: 'input',
-      message: 'How should user contribute?',
+      message: 'How should users contribute?',
       name: 'contributing',    
     },
     {
       type: 'input',
       message: 'Run some tests.',
-      name: 'test',    
+      name: 'tests',    
     },
     {
       type: 'input',
